6dff02960c0ee59a74051758326d536d5091d94b
Utkarsh <Utkarsh@Utkarsh-PC.Dlink>
2019-05-11 23:24:34 +0530

    added binary tree inOrder traversal using stacks

--- src/dataStructuresImplementations/BinaryTreeTraversal.java
+++ src/dataStructuresImplementations/BinaryTreeTraversal.java
@@ -1,5 +1,7 @@
 package dataStructuresImplementations;
 
+import java.util.Stack;
+
 /**
  * InOrder is left root right
  * PreOrder is root left right
@@ -9,12 +11,71 @@ package dataStructuresImplementations;
  */
 public class BinaryTreeTraversal {
 	
+	/**
+	 * Simple recursive solution
+	 * Runtime: O(n)
+	 * @param root
+	 */
 	static void printInOrder(Node root){
 		if(root == null)
 			return;
 		printInOrder(root.left);
 		System.out.print(root.val+" ");
 		printInOrder(root.right);
+	}
+	
+	/**
+	 * print inOrder using stack with single while loop for
+	 * reaching all the nodes.
+	 * Runtime: O(n)
+	 * @param root
+	 */
+	static void printInOrderUsingStack(Node root) {
+		if(root == null)
+			return;
+		Stack<Node> stack = new Stack<Node>();
+		stack.push(root);
+		Node tmp = root.left;
+		while(tmp!=null) {
+			if(tmp.left!=null) {
+				stack.push(tmp);
+				tmp = tmp.left;
+				continue;
+			} else {
+				System.out.print(tmp.val+" ");
+			}
+			
+			if(!stack.isEmpty()) {
+				Node cur = stack.pop();
+				System.out.print(cur.val+" ");
+				tmp = cur.right;
+			}else // only reaches here after hitting all the elements
+				tmp = null;
+		}
+	}
+	
+	/**
+	 * prints inOrder using stack and inner while loop to
+	 * get to the left most node
+	 * Runtime: O(n) , even though it looks like double loop, it is just 
+	 * going through left elements right away before processing further
+	 */
+	static void printInOrderUsingStackLoopy(Node root) {
+		if(root == null)
+			return;
+		Stack<Node> stack = new Stack<Node>();
+		Node cur = root;
+		while(cur!=null || !stack.isEmpty()) {
+			// keep pushing until left most node reached
+			while(cur!=null) {
+				stack.push(cur);
+				cur = cur.left;
+			}
+			cur = stack.pop();
+			System.out.print(cur+" ");
+			cur = cur.right;
+			
+		}
 	}
 	
 	static void printPreOrder(Node root){
@@ -41,6 +102,11 @@ public class BinaryTreeTraversal {
 		public Node(int num){
 			this.val = num;
 		}
+		
+		@Override
+		public String toString() {
+			return String.valueOf(this.val);
+		}
 	}
 	
 	static class BinaryTree{
@@ -62,13 +128,18 @@ public class BinaryTreeTraversal {
 		bt.root.right.left = new Node(6);
 		bt.root.right.right = new Node(8);
 		System.out.println("SET 1");
-		System.out.println("print InOrder: ");
+		System.out.println("print InOrder using recursion: ");
 		printInOrder(bt.root);
+		System.out.println("\nPrint InOrder using stack (single loop): ");
+		printInOrderUsingStack(bt.root);
+		System.out.println("\nPrint InOrder using stack (double loop): ");
+		printInOrderUsingStackLoopy(bt.root);
+		
 		System.out.println("\nprint PreOrder: ");
 		printPreOrder(bt.root);
 		System.out.println("\nprint PostOrder: ");
 		printPostOrder(bt.root);
-		
+
 		// test set 2
 		BinaryTree tree = new BinaryTree(1); 
         tree.root.left = new Node(2); 
@@ -76,8 +147,13 @@ public class BinaryTreeTraversal {
         tree.root.left.left = new Node(4); 
         tree.root.left.right = new Node(5); 
         System.out.println("\n\nSET 2");
-		System.out.println("print InOrder: ");
+		System.out.println("print InOrder using recursion: ");
 		printInOrder(tree.root);
+		System.out.println("\nPrint InOrder using stack (single loop): ");
+		printInOrderUsingStack(tree.root);
+		System.out.println("\nPrint InOrder using stack (double loop): ");
+		printInOrderUsingStackLoopy(tree.root);
+		
 		System.out.println("\nprint PreOrder: ");
 		printPreOrder(tree.root);
 		System.out.println("\nprint PostOrder: ");
